b337def2e6a1c8ccc891bfc787d75cc8a4650298
Søren Louv-Jansen <sorenlouv@gmail.com>
2016-10-25 23:56:15 +0200

    Update dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,11 @@
-import { Router, Route, useRouterHistory } from 'react-router';
+import { Router, Route, hashHistory } from 'react-router';
 import React from 'react';
 import { render } from 'react-dom';
 import App from './modules/App.jsx';
-import { createHashHistory } from 'history';
 import './styles/app.scss';
 
-const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 render(
-	<Router history={appHistory}>
+	<Router history={hashHistory}>
 		<Route path='/(:pullRequestNumber)' component={App} />
 	</Router>,
 	document.getElementById('app')
